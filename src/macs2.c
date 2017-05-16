f2c449a26c2ed5ef9c56d77d2c7a3811cdb1746f
Jeroen Meijer <jjgmeijer@gmail.com>
2017-05-16 20:37:00 +0200

    Code for parsing MCC formulas

--- src/macs2.c
+++ src/macs2.c
@@ -8,6 +8,8 @@
 #include <andl-lexer.h>
 #include <macs2-andl-parser.h>
 #include <util.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 /**
  * Load the andl file in \p name.
@@ -85,6 +87,147 @@ do_macs2_things(andl_context_t *andl_context)
     warn("There are %d out arcs", andl_context->num_out_arcs);
 }
 
+static int
+parse_formula(xmlNode *node)
+{
+    int res = 0;
+    // first check if the node is not a NULL pointer.
+    if (node == NULL) {
+        res = 1;
+        warn("Invalid XML");
+    // only parse xml nodes, skip other parts of the XML file.
+    } else if (node->type != XML_ELEMENT_NODE) res = parse_formula(xmlNextElementSibling(node));
+    // parse forAll
+    else if (xmlStrcmp(node->name, (const xmlChar*) "all-paths") == 0) {
+        fprintf(stderr, "A ");
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse Exists
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "exists-path") == 0) {
+        fprintf(stderr, "E ");
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse Globally
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "globally") == 0) {
+        fprintf(stderr, "G ");
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse Finally
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "finally") == 0) {
+        fprintf(stderr, "F ");
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse neXt
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "next") == 0) {
+        fprintf(stderr, "X ");
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse Until
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "until") == 0) {
+        fprintf(stderr, "(");
+        res = parse_formula(xmlFirstElementChild(node));
+        fprintf(stderr, ") U (");
+        res |= parse_formula(xmlNextElementSibling(xmlFirstElementChild(node)));
+        fprintf(stderr, ")");
+    // parse before
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "before") == 0) {
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse reach
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "reach") == 0) {
+        res = parse_formula(xmlFirstElementChild(node));
+    // parse negation
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "negation") == 0) {
+        fprintf(stderr, "!(");
+        res = parse_formula(xmlFirstElementChild(node));
+        fprintf(stderr, ")");
+    // parse conjunction
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "conjunction") == 0) {
+        fprintf(stderr, "(");
+        res = parse_formula(xmlFirstElementChild(node));
+        fprintf(stderr, ") && (");
+        res |= parse_formula(xmlNextElementSibling(xmlFirstElementChild(node)));
+        fprintf(stderr, ")");
+    // parse disjunction
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "disjunction") == 0) {
+        fprintf(stderr, "(");
+        res = parse_formula(xmlFirstElementChild(node));
+        fprintf(stderr, ") || (");
+        res |= parse_formula(xmlNextElementSibling(xmlFirstElementChild(node)));
+        fprintf(stderr, ")");
+    // parse is-fireable: atomic predicate!
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "is-fireable") == 0) {
+        fprintf(stderr, "is-fireable(");
+        res = parse_formula(xmlFirstElementChild(node));
+        fprintf(stderr, ")");
+    // parse transition (part of the atomic predicate)
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "transition") == 0) {
+        for (xmlNode *transition = node; transition != NULL;
+                transition = xmlNextElementSibling(transition)) {
+            fprintf(stderr, "%s,", xmlNodeGetContent(transition));
+        }
+    } else {
+        res = 1;
+        warn("Invalid xml node '%s'", node->name);
+    }
+    return res;
+}
+
+static int
+parse_xml(xmlNode *node)
+{
+    int res = 0;
+    // first check if the node is not a NULL pointer.
+    if (node == NULL) {
+        res = 1;
+        warn("Invalid XML");
+    // only parse xml nodes, skip other parts of the XML file.
+    } else if (node->type != XML_ELEMENT_NODE) res = parse_xml(xmlNextElementSibling(node));
+    // parse property-set
+    else if (xmlStrcmp(node->name, (const xmlChar*) "property-set") == 0) {
+        // loop over all children that are property nodes
+        for (xmlNode *property = xmlFirstElementChild(node);
+                property != NULL && !res;
+                property = xmlNextElementSibling(property)) {
+            res = parse_xml(property);
+        }
+    // parse property
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "property") == 0) {
+        warn("parsing property");
+        res = parse_xml(xmlFirstElementChild(node));
+    // parse id of property
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "id") == 0) {
+        warn("Property id is: %s", xmlNodeGetContent(node));
+        res = parse_xml(xmlNextElementSibling(node));
+    // parse description of property
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "description") == 0) {
+        warn("Property description is: %s", xmlNodeGetContent(node));
+        res = parse_xml(xmlNextElementSibling(node));
+    // parse the formula
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "formula") == 0) {
+        warn("Parsing formula...");
+        res = parse_formula(xmlFirstElementChild(node));
+        printf("\n");
+    // node not recognized
+    } else {
+        res = 1;
+        warn("Invalid xml node '%s'", node->name);
+    }
+
+    return res;
+}
+
+static int
+load_xml(const char* name)
+{
+    int res;
+
+    LIBXML_TEST_VERSION
+    warn("parsing formulas file: %s", name);
+    xmlDoc *doc = xmlReadFile(name, NULL, 0);
+    if (doc == NULL) res = 1;
+    else {
+        xmlNode *node = xmlDocGetRootElement(doc);
+        res = parse_xml(node);
+    }
+
+    return res;
+}
+
 /**
  * \brief main.
  *
@@ -93,19 +236,24 @@ do_macs2_things(andl_context_t *andl_context)
 int main(int argc, char** argv)
 {
     int res;
-    if (argc == 2) {
+    if (argc >= 2) {
         andl_context_t andl_context; 
         const char *name = argv[1];
         res = load_andl(&andl_context, name);
         if (res) warn("Unable to parse file '%s'", name);
         else {
             warn("Successful parse of file '%s' :)", name);
+            if (argc == 3) {
+                const char *formulas = argv[2];
+                res = load_xml(formulas);
+                if (res) warn("Unable to load xml '%s'", formulas);
+            }
             init_sylvan();
             do_macs2_things(&andl_context);
             deinit_sylvan();
         }
     } else {
-        warn("Usage: %s <petri-net>.andl", argv[0]);
+        warn("Usage: %s <petri-net>.andl [<CTL-formulas>.xml]", argv[0]);
         res = 1;
     }
 
