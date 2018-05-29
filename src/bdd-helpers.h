66c4b2d4d98e06899eb04888fa2f64c3a3d663c3
Nander Voortman <nander@nander.net>
2018-05-29 14:37:18 +0200

    Small chagnes

--- src/bdd-helpers.h
+++ src/bdd-helpers.h
@@ -3,3 +3,4 @@
 /* quickly export export a bdd to a dot-file
 */
 void export_bdd(BDD bdd, int fnumber);
+BDD rel_prod(BDD states, BDD relation, BDD x, BDDMAP map);
--- src/checkers.c
+++ src/checkers.c
@@ -3,8 +3,16 @@
 // TODO: Prev function
 BDD MYprev(BDD in, BDD relations[], int n_relations, BDD x, BDDMAP map) {
     LACE_ME;
+    BDD result = sylvan_false;
+    for(int i=0; i < n_relations; i++) {
+        BDD relation = relations[i];
+        // execute relation check; simply try running relations in reverse.
 
-    return in;
+        BDD relation_result  =  sylvan_relprev(relation, in, x);
+
+        result = sylvan_or(result, relation_result);
+    }
+    return result;
 }
 
 BDD checkEU(BDD left, BDD right, BDD relations[], int n_relations, BDD x, BDDMAP map) 
@@ -28,7 +36,8 @@ BDD checkEG(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map)
         old = z;
         z = sylvan_and(z, MYprev(z, relations, n_relations,  x, map ));
     }
-    return z;}
+    return z;
+}
 
 BDD checkEX(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map) 
 {
@@ -37,7 +46,7 @@ BDD checkEX(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map)
 }
 
 BDD checkEF(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map) {
- return checkEU(sylvan_true, left, relations, n_relations,  x, map );
+    return checkEU(sylvan_true, left, relations, n_relations,  x, map );
 }
 
 BDD checkAF(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map) {
@@ -45,6 +54,7 @@ BDD checkAF(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map) {
     im1 = checkEG(im1, relations, n_relations,  x, map );
     return sylvan_not(im1);
 }
+
 BDD checkAG(BDD left, BDD relations[], int n_relations, BDD x, BDDMAP map) {
     BDD im1 = sylvan_not(left);
     im1 = checkEF(im1, relations, n_relations,  x, map );
